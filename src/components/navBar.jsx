2417c19eb9052b68b3d6b45d5b899c69ace3c2d8
Liam Paul <liampaul144@gmail.com>
2021-02-15 14:44:10 -0800

    css expermenting

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,23 +1,18 @@
 import { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <Link to="/" className="navbar-brand">Pop-Stack</Link>
-
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <il className="nav-item">
-              <Link to="/portfolio" className="nav-link">Portfolio</Link>
-            </il>
-            <il className="nav-item">
-              <Link className="nav-link">2</Link>
-            </il>
-            <il className="nav-item">
-              <Link className="nav-link">3</Link>
-            </il>
+      <nav id="navbar">
+        <div className="">
+          <ul className="">
+            <li className="">
+              <Link to="">Pop-Stack</Link>
+            </li>
+            <li className="">
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
           </ul>
         </div>
       </nav>
--- src/home.jsx
+++ src/home.jsx
@@ -4,15 +4,16 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Call To Action</h1>
-        <Link to="/contactme">Contact me</Link>
-
-        <hr/>
-
-        <h1>About</h1>
-        <p>This is the first draft of my home page</p>
-        <a href="http://github.com/r1ri">Github</a>
+      <div className="container">
+        <div className="box-1">
+          <h1>Call To Action</h1>
+          <Link to="/contactme">Contact me</Link>
+        </div>
+        <div className="box-2">
+          <h1>About</h1>
+          <p>This is the first draft of my home page</p>
+          <a href="http://github.com/r1ri">Github</a>
+        </div>
       </div>
     );
   }
