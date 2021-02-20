900a99b71cd2f0f9019384cb85256f104384cab3
Liam Paul <liam@pop-stack.com>
2021-02-20 07:02:02 -0800

    actual navbar fix

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,13 +1,21 @@
 import { Component } from "react";
 import { NavLink } from "react-router-dom";
-import './navbar.css'
+import "./navbar.css";
 
 class Navbar extends Component {
   render() {
     return (
       <nav id="navbar">
-        <NavLink id="logo" to="/">Pop-Stack</NavLink>
-        <NavLink to="/portfolio">Portfolio</NavLink>
+        <ul>
+          <li>
+            <NavLink id="logo" to="/">
+              Pop-Stack
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio">Portfolio</NavLink>
+          </li>
+        </ul>
       </nav>
     );
   }
